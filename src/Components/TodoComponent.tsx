14a907f724e5b2b71903831b3fc5309c51ed50f7
JayasriEsuru <ejayasri98reddy@gmail.com>
2024-05-04 13:26:58 +0530

    create and update FE changes

---
+++ src/Components/TodoComponent.tsx
@@ -0,0 +1,136 @@
+import { Button, Table } from "antd";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import TodoModal from "./TodoModal";
+
+const TodoComponent = () => {
+  const [visible, setVisible] = useState(false);
+  const [data, setData] = useState([]);
+  const [currentItem, setCurrentItem] = useState(null);
+
+  const showModal = () => {
+    setVisible(true);
+    setCurrentItem(null)
+  };
+
+  useEffect(() => {
+    showAll(); // Fetch data when component mounts
+  }, []);
+
+  const handleOk = (data: any) => {
+    console.log({currentItem})
+    
+const reqBody = {activity: data.activity, CompleteBy: data.CompleteBy}
+    if(!currentItem){
+      axios
+      .post("http://localhost:5555/listItems/create", reqBody)
+      .then((response) => {
+        setVisible(false);
+        showAll();
+      })
+      .catch((error) => {
+        // Handle any errors here
+        console.error("Error creating to-do:", error);
+      });
+    }else {
+      axios
+      .post(`http://localhost:5555/listItems/update/${data.id}`, data)
+      .then((response) => {
+        setVisible(false);
+        setCurrentItem(null)
+        showAll();
+      })
+      .catch((error) => {
+        // Handle any errors here
+        console.error("Error creating to-do:", error);
+      });
+    }
+  };
+
+  const showAll = () => {
+    axios
+      .post("http://localhost:5555/listItems/readAll")
+      .then((response) => {
+        setData(response.data); // Update component state with fetched data
+      })
+      .catch((err) => {
+        console.log(err); // Log any errors that occur during the fetch
+      });
+  };
+
+  const columns = [
+    {
+      title: "S.No",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "activity",
+      dataIndex: "activity",
+      key: "activity",
+    },
+    {
+      title: "status",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "CompleteBy",
+      dataIndex: "CompleteBy",
+      key: "CompleteBy",
+    },
+    {
+      title: "startedDate",
+      dataIndex: "startedDate",
+      key: "startedDate",
+    },
+    {
+      title: "startedTime",
+      dataIndex: "startedTime",
+      key: "startedTime",
+    },
+    {
+      title: "completedDate",
+      dataIndex: "completedDate",
+      key: "completedDate",
+    },
+    {
+      title: "completionTime",
+      dataIndex: "completionTime",
+      key: "completionTime",
+    },
+    {
+      render: (item: any) => (
+        <Button
+          onClick={() => {
+            setVisible(true);
+            setCurrentItem(item);
+          }}
+        >
+          Update
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <Button
+        type="primary"
+        style={{ marginLeft: "45%", marginTop: "50px" }}
+        onClick={showModal}
+      >
+        Create A TO DO
+      </Button>
+      <TodoModal
+        visible={visible}
+        setVisible={setVisible}
+        handleOk={handleOk}
+        currentItem={currentItem}
+      />
+      <Table dataSource={data} columns={columns} />
+    </>
+  );
+};
+
+export default TodoComponent;
---
+++ src/Components/TodoModal.tsx
@@ -0,0 +1,217 @@
+import { Input, Modal } from "antd";
+import React, { useEffect, useState } from "react";
+
+export interface TodoModalProps {
+  visible: boolean;
+  setVisible: any;
+  handleOk: any;
+  currentItem?: any;
+}
+
+interface DataInterface {
+  activity?: string;
+  CompleteBy?: string;
+  status?: string;
+  startedDate?: string;
+  startedTime?: string;
+  id?: string;
+  completedDate?: string;
+  completedTime?: string;
+}
+
+const defaultData = {
+  activity: "",
+  CompleteBy: "",
+  status: "",
+  id: "",
+  startedDate: "",
+  startedTime: "",
+  completedDate: "",
+  completedTime: "",
+};
+
+const TodoModal: React.FC<TodoModalProps> = ({
+  visible,
+  setVisible,
+  handleOk,
+  currentItem,
+}) => {
+  const [updatedData, setUpdatedData] = useState<DataInterface | null>(
+    defaultData
+  );
+  // const [data, setData] = useState<DataInterface | null>(null);
+
+  useEffect(() => {
+    if (currentItem) {
+      if (currentItem?.status === "open") {
+        setUpdatedData({ ...currentItem, status: "inprogress" });
+      } else {
+        setUpdatedData({ ...currentItem, status: "completed" });
+      }
+    }
+  }, [currentItem]);
+
+  const onOkClick = () => {
+    if (updatedData?.activity !== "" && updatedData?.CompleteBy !== "") {
+      console.log({ updatedData });
+      handleOk(updatedData);
+      setUpdatedData(null);
+    }
+  };
+
+  const onInputChange = (e: any, key: string) => {
+    setUpdatedData({ ...updatedData, [key]: e.target.value });
+  };
+
+  return (
+    <Modal
+      title="ToDo"
+      visible={visible}
+      onOk={onOkClick}
+      onCancel={() => {
+        setVisible(false);
+        setUpdatedData(null);
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          margin: "25px",
+          marginLeft: "20%",
+          width: "50%",
+        }}
+      >
+        <label style={{ width: "30%" }}>Activity:</label>
+        <Input
+          placeholder="Enter Activity"
+          value={updatedData?.activity}
+          onChange={(e) => onInputChange(e, "activity")}
+          disabled={currentItem}
+        />
+      </div>
+
+      {updatedData?.status ? (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "20px",
+            margin: "25px",
+            marginLeft: "20%",
+            width: "50%",
+          }}
+        >
+          <label style={{ width: "30%" }}>Status:</label>
+          <Input
+            value={updatedData?.status}
+            onChange={(e) => onInputChange(e, "status")}
+          />
+        </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "20px",
+            margin: "25px",
+            marginLeft: "20%",
+            width: "50%",
+          }}
+        >
+          <label style={{ width: "30%" }}>Complete by Date:</label>
+          <Input
+            type="date"
+            placeholder="Enter Date"
+            value={updatedData?.CompleteBy}
+            onChange={(e) => onInputChange(e, "CompleteBy")}
+          />
+        </div>
+      )}
+
+      {updatedData?.status === "inprogress" && (
+        <>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "20px",
+              margin: "25px",
+              marginLeft: "20%",
+              width: "50%",
+            }}
+          >
+            <label style={{ width: "30%" }}>Started Date:</label>
+            <Input
+              type="date"
+              placeholder="Enter Start Date"
+              value={updatedData?.startedDate}
+              onChange={(e) => onInputChange(e, "startedDate")}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "20px",
+              margin: "25px",
+              marginLeft: "20%",
+              width: "50%",
+            }}
+          >
+            <label style={{ width: "30%" }}>Started Time:</label>
+            <Input
+              type="time"
+              placeholder="Enter Start Time"
+              value={updatedData?.startedTime}
+              onChange={(e) => onInputChange(e, "startedTime")}
+            />
+          </div>
+        </>
+      )}
+
+      {updatedData?.status === "completed" && (
+        <>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "20px",
+              margin: "25px",
+              marginLeft: "20%",
+              width: "50%",
+            }}
+          >
+            <label style={{ width: "30%" }}>Completed Date:</label>
+            <Input
+              type="date"
+              placeholder="Enter Completed Date"
+              value={updatedData?.completedDate}
+              onChange={(e) => onInputChange(e, "completedDate")}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "20px",
+              margin: "25px",
+              marginLeft: "20%",
+              width: "50%",
+            }}
+          >
+            <label style={{ width: "30%" }}>Completed Time:</label>
+            <Input
+              type="time"
+              placeholder="Enter Completed Time"
+              value={updatedData?.completedTime}
+              onChange={(e) => onInputChange(e, "completedTime")}
+            />
+          </div>
+        </>
+      )}
+    </Modal>
+  );
+};
+
+export default TodoModal;
